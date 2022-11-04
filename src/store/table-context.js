375d181eabf6e7888a4c301160faf26f1b1da291
Anastasiyaaaa <anastasiya.onishchenk@gmail.com>
2022-11-04 15:33:06 +0200

    add extra page logic for data and provider

--- src/store/table-context.js
+++ src/store/table-context.js
@@ -4,6 +4,7 @@ import React from 'react';
 const TableContext = React.createContext({
     currentPage: '',
     mainPage: {},
+    extraPage: {},
     updateCurrentPage: (page) => {},
     sortColumnFunction: (name, elementType, sortType) => {},
     hideShowColumnFunction: (name, visible) => {},
